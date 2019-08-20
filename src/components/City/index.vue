cda307320c3e93ee2b44b0f4630f3e52ccdb8c6c
dhhjk <543075717@qq.com>
2019-08-20 14:01:51 +0800

    init

--- src/components/City/index.vue
+++ src/components/City/index.vue
@@ -1,84 +1,24 @@
 <template>
-
 			<div class="city_body">
 				<div class="city_list">
 					<div class="city_hot">
 						<h2>热门城市</h2>
 						<ul class="clearfix">
-							<li>上海</li>
-							<li>北京</li>
-							<li>上海</li>
-							<li>北京</li>
-							<li>上海</li>
-							<li>北京</li>
-							<li>上海</li>
-							<li>北京</li>
+							<li v-for="item in hotList" :key="item.id">{{item.nm}}</li>
 						</ul>
 					</div>
-					<div class="city_sort">
-						<div>
-							<h2>A</h2>
+					<div class="city_sort" ref="city_sort">
+						<div v-for="item in cityList" :key="item.id">
+							<h2>{{item.index}}</h2>
 							<ul>
-								<li>阿拉善盟</li>
-								<li>鞍山</li>
-								<li>安庆</li>
-								<li>安阳</li>
+								<li v-for="itemList in item.list" :key="itemList.id">{{itemList.nm}}</li>
 							</ul>
 						</div>
-						<div>
-							<h2>B</h2>
-							<ul>
-								<li>北京</li>
-								<li>保定</li>
-								<li>蚌埠</li>
-								<li>包头</li>
-							</ul>
-						</div>
-						<div>
-							<h2>A</h2>
-							<ul>
-								<li>阿拉善盟</li>
-								<li>鞍山</li>
-								<li>安庆</li>
-								<li>安阳</li>
-							</ul>
-						</div>
-						<div>
-							<h2>B</h2>
-							<ul>
-								<li>北京</li>
-								<li>保定</li>
-								<li>蚌埠</li>
-								<li>包头</li>
-							</ul>
-						</div>
-						<div>
-							<h2>A</h2>
-							<ul>
-								<li>阿拉善盟</li>
-								<li>鞍山</li>
-								<li>安庆</li>
-								<li>安阳</li>
-							</ul>
-						</div>
-						<div>
-							<h2>B</h2>
-							<ul>
-								<li>北京</li>
-								<li>保定</li>
-								<li>蚌埠</li>
-								<li>包头</li>
-							</ul>
-						</div>	
 					</div>
 				</div>
 				<div class="city_index">
 					<ul>
-						<li>A</li>
-						<li>B</li>
-						<li>C</li>
-						<li>D</li>
-						<li>E</li>
+						<li v-for="(item,index) in cityList" :key="item.index" @touchstart="handleToIndex(index)">{{item.index}}</li>
 					</ul>
 				</div>
 			</div>
@@ -86,7 +26,77 @@
 
 <script>
 export default {
-    name: "City"
+	name: "City",
+	data() {
+		return {
+			cityList: [],
+			hotList: []
+		}
+	},
+	mounted() {
+		this.axios.get('/api/cityList').then((res) => {
+			// console.log(res);	
+			var msg = res.data.msg;
+			if(msg === 'ok') {
+				var cities = res.data.data.cities;
+				var {cityList,hotList} = this.formatCityList(cities);
+				this.cityList = cityList;
+				this.hotList = hotList;
+			}	
+		})
+	},
+	methods: {
+		formatCityList(cities) {
+			var cityList = [];
+			var hotList = [];
+			for(var i=0;i<cities.length;i++) {
+				if(cities[i].isHot === 1) {
+					hotList.push(cities[i])
+				}
+			}
+			for(var i=0;i<cities.length;i++) {
+				var firstLetter = cities[i].py.substring(0,1).toUpperCase()
+				if(toCom(firstLetter)) {
+					cityList.push({index:firstLetter,list:[{nm:cities[i].nm,id:cities[i].id}]})
+				}else {
+					for(var j=0;j<cityList.length;j++) {
+						if(cityList[j].index === firstLetter) {
+							cityList[j].list.push({nm:cities[i].nm,id:cities[i].id})
+						}
+					}
+				}
+			}
+
+			cityList.sort((n1,n2) => {
+				if (n1.index > n2.index) {
+					return 1
+				}else if(n1.index < n2.index) {
+					return -1
+				}else {
+					return 0
+				}
+			})
+
+			function toCom(firstLetter) {
+				for(var i=0;i<cityList.length;i++) {
+					if (cityList[i].index === firstLetter) {
+						return false
+					}
+				}
+				return true
+			}
+			// console.log(cityList);
+			// console.log(hotList);
+			return {
+				cityList,
+				hotList
+			}
+		},
+		handleToIndex(index) {
+			var h2 = this.$refs.city_sort.getElementsByTagName("h2");
+			this.$refs.city_sort.parentNode.scrollTop = h2[index].offsetTop;
+		}
+	},
 }
 </script>
 
