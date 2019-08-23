ac8dd6917cbab9a44874226b50507ee2cc9c0ca2
dhhjk <543075717@qq.com>
2019-08-23 15:40:44 +0800

    finish

--- src/routers/index.js
+++ src/routers/index.js
@@ -8,7 +8,8 @@ Vue.use(Router)
 
 export default new Router({
   mode: 'history',
-  base: process.env.BASE_URL,
+  // base: process.env.BASE_URL,
+  base: 'miaomiao',
   routes: [
     movieRouter,
     mineRouter,
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+    publicPath: '/miaomiao',
     devServer: {
         proxy: {
             "/api": {
